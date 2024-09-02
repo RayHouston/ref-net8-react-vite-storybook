5196fe8b60423e883b7bb20f45aff4635543518f
Ray Houston <ray.houston@gmail.com>
2024-09-01 19:36:54 -0500

    adding theme to components

--- web.client.components/.storybook/preview.ts
+++ web.client.components/.storybook/preview.ts
@@ -1,5 +1,14 @@
 import type { Preview } from "@storybook/react";
 
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+import { dylTheme, materialUiTheme } from "../src/themes/theme";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 const preview: Preview = {
     parameters: {
         controls: {
@@ -9,6 +18,18 @@ const preview: Preview = {
             },
         },
     },
+
+    decorators: [
+        withThemeFromJSXProvider({
+            GlobalStyles: CssBaseline,
+            Provider: ThemeProvider,
+            themes: {
+                dyl: dylTheme,
+                materialUiTheme: materialUiTheme,
+            },
+            defaultTheme: "dyl",
+        }),
+    ],
 };
 
 export default preview;
